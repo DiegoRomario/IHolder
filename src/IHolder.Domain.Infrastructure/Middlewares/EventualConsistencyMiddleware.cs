cb1cd2f6cbd952f747f65b7a7a5c68f95525f20b
DiegoRomario <diego.romario@outlook.com>
2024-07-02 07:08:36 -0300

    add eventual consistency middleware

---
+++ src/IHolder.Domain.Infrastructure/Middlewares/EventualConsistencyMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using IHolder.Domain.Common;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace IHolder.Domain.Infrastructure.Middlewares;
+
+public class EventualConsistencyMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext context, IPublisher publisher, IHolderDbContext dbContext)
+    {
+        var transaction = await dbContext.Database.BeginTransactionAsync();
+
+        context.Response.OnCompleted(async () =>
+        {
+            try
+            {
+                if (context.Items.TryGetValue("DomainEventsQueue", out var value) && value is Queue<IDomainEvent> domainEventsQueue)
+                {
+                    while (domainEventsQueue!.TryDequeue(out var domainEvent))
+                    {
+                        await publisher.Publish(domainEvent);
+                    }
+                }
+
+                await transaction.CommitAsync();
+            }
+            catch (Exception)
+            {
+                //  TODO: NOTIFY THE CLIENT THAT EVEN THOUGH THEY GOT A GOOD RESPONSE, THE CHANGES DIDN'T TAKE PLACE DUE TO AN UNEXPECTED ERROR
+            }
+            finally
+            {
+                await transaction.DisposeAsync();
+            }
+
+        });
+
+        await _next(context);
+    }
+}
