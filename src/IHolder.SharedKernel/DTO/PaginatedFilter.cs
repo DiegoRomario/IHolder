a989188797e3a140a6abf76ccb7c3392964f718b
DiegoRomario <diego.romario@outlook.com>
2024-07-20 10:27:47 -0300

    scaffold shared kernel project

---
+++ src/IHolder.SharedKernel/DTO/PaginatedFilter.cs
@@ -0,0 +1,22 @@
+﻿namespace IHolder.SharedKernel.DTO;
+
+public record PaginatedFilter
+{
+    private const short MinPageSize = 0;
+    private const short MinPageNumber = 1;
+    private const short DefaultPageSize = 10;
+
+    public int PageNumber { get; init; }
+    public short PageSize { get; init; }
+
+    public PaginatedFilter(int pageNumber = MinPageNumber, short pageSize = DefaultPageSize)
+    {
+        PageNumber = EnsurePageNumberInRange(pageNumber);
+        PageSize = EnsurePageSizeInRange(pageSize);
+    }
+
+    private static int EnsurePageNumberInRange(int pageNumber) => pageNumber <= 0 ? MinPageNumber : pageNumber;
+
+    private static short EnsurePageSizeInRange(short pageSize) => pageSize < MinPageSize ? MinPageSize : pageSize;
+}
+
---
+++ src/IHolder.SharedKernel/DTO/PaginatedList.cs
@@ -0,0 +1,8 @@
+﻿namespace IHolder.SharedKernel.DTO;
+
+public record PaginatedList<T>(IReadOnlyList<T> Items, int TotalCount, int PageNumber, short PageSize)
+{
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+}
