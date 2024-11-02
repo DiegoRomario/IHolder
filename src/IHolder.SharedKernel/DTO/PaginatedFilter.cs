d71059014b6e5c3d7b014b557831b667fb140469
DiegoRomario <diego.romario@outlook.com>
2024-11-02 18:17:08 -0300

    fix min page size in paginated filter request

--- src/IHolder.SharedKernel/DTO/PaginatedFilter.cs
+++ src/IHolder.SharedKernel/DTO/PaginatedFilter.cs
@@ -2,7 +2,7 @@
 
 public record PaginatedFilter
 {
-    private const short MinPageSize = 0;
+    private const short MinPageSize = 1;
     private const short MinPageNumber = 1;
     private const short DefaultPageSize = 10;
 
@@ -11,12 +11,11 @@ public record PaginatedFilter
 
     public PaginatedFilter(int pageNumber = MinPageNumber, short pageSize = DefaultPageSize)
     {
-        PageNumber = EnsurePageNumberInRange(pageNumber);
-        PageSize = EnsurePageSizeInRange(pageSize);
+        PageNumber = ValidatePageNumber(pageNumber);
+        PageSize = ValidatePageSize(pageSize);
     }
 
-    private static int EnsurePageNumberInRange(int pageNumber) => pageNumber <= 0 ? MinPageNumber : pageNumber;
+    private static int ValidatePageNumber(int pageNumber) => pageNumber < MinPageNumber ? MinPageNumber : pageNumber;
 
-    private static short EnsurePageSizeInRange(short pageSize) => pageSize < MinPageSize ? MinPageSize : pageSize;
+    private static short ValidatePageSize(short pageSize) => pageSize < MinPageSize ? DefaultPageSize : pageSize;
 }
-
